93b4b9de4f502de1eb1895989b43d81c7db8ecde
CloudLearningrameshbabum <rameshbabu.parexel@gmail.com>
2025-05-16 13:45:37 +0530

    Automation for Easy Life Created

--- Automation1.sh
+++ Automation1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 THRESHOLD=500 # in MB
 AVAILABLE=$(free -m | awk '/Mem/ {print $7}')
+echo "Memory available details : $AVAILABLE"
 if [ $AVAILABLE -lt $THRESHOLD ]; then
-    echo "Low Memory Alert!"
+    echo "Low Memory Alert! Please have a glance"
 fi

4781a8a0400b6e2dadb433a213e2d08411ec88a8
CloudLearningrameshbabum <rameshbabu.parexel@gmail.com>
2025-05-17 13:55:41 +0530

    Automation for Easy Life Created

---
+++ Colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Colors
+RED="\033[0;31m"
+echo "RED"
+
+GREEN="\033[0;32m"
+echo "GREEN"
+
+YELLOW="\033[1;33m"
+echo "YELLOW"
+
+BLUE="\033[0;34m"
+echo "BLUE"
+
+NC="\033[0m" # No Color
+echo "NC"
